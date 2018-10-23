d1ce261dd01b16028d49883bcbf6e9467fab8453
Alessandro Perna <almavivadeveloper@MacBook-Pro-di-AlePerna.local>
2018-10-23 17:11:45 +0200

    first commit

---
+++ CollectionViewPinterest/PinterestLayout.swift
@@ -0,0 +1,13 @@
+//
+//  PinterestCollectionViewLayout.swift
+//  CollectionViewPinterest
+//
+//  Created by Alessandro Perna on 05/06/18.
+//  Copyright © 2018 VJTechnology Srl. All rights reserved.
+//
+
+import UIKit
+
+class PinterestCollectionViewLayout: UICollectionViewLayout {
+
+}
